0dd48a5414c94f3487ebbd2a4536fdda2dbb64b2
Anzhalika Turlak <293852647@mail.ru>
2024-10-05 02:23:07 +0400

    feat: add third app

---
+++ apps/js-learn/src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App: React.FC = () => <h1>Hello, Third App with Rush.js!</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
